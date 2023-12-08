1f4413e1f820b1f8ce0b248f58e47645588e2273
iWas-Coder <wasymatieh01@gmail.com>
2023-12-08 02:44:48 +0100

    Templates

--- cli/cmd/new.go
+++ cli/cmd/new.go
@@ -19,14 +19,25 @@ type Config struct {
 var (
   cfg Config
   cfgFile string
+  tpl = "github.com/iWas-Coder/"
   newCmd = &cobra.Command{
     Use:   "new",
     Short: "Create a game powered by WGE",
     Long:  `Initialize a project with either an interactive form or via a YAML configuration file where all needed game properties are specified.`,
     Run: func(cmd *cobra.Command, args []string) {
       if cfgFile != "" {
+        if len(args) != 0 {
+          fmt.Println("[ERROR]: Passing arguments alongside a config file is not compatible")
+          os.Exit(1)
+        }
         parseCfgFile()
       } else {
+        if len(args) > 1 {
+          fmt.Println("[ERROR]: Invalid number of arguments")
+          os.Exit(1)
+        } else if len(args) == 1 {
+          cfg.ProjectName = args[0]
+        }
         form()
       }
       showCfg()
@@ -42,33 +53,39 @@ func showCfg() {
   yamlData, err := yaml.Marshal(&cfg)
   if err != nil {
     fmt.Printf("[ERROR]: YAML marshaling failed %v\n", err)
-    return
+    os.Exit(1)
   }
   fmt.Println(string(yamlData))
+  if cfg.GeometryType == "2D" {
+    tpl += "wge-2d-game-template"
+  } else if cfg.GeometryType == "3D" {
+    tpl += "wge-3d-game-template"
+  }
+  fmt.Printf("Selected template: %s\n", tpl)
 }
 
 func parseCfgFile() {
   _, err := os.Stat(cfgFile)
   if err != nil {
     fmt.Printf("[ERROR]: Config file does not exist (%v)\n", err)
-    return
+    os.Exit(1)
   }
   data, err := ioutil.ReadFile(cfgFile)
   if err != nil {
     fmt.Printf("[ERROR]: Config file could not be read (%v)\n", err)
-    return
+    os.Exit(1)
   }
   err = yaml.Unmarshal(data, &cfg)
   if err != nil {
     fmt.Printf("[ERROR]: Config file could not be deserialized (%v)\n", err)
-    return
+    os.Exit(1)
   }
   v := reflect.ValueOf(cfg)
   for i := 0; i < v.NumField(); i++ {
     fieldValue := v.Field(i).Interface()
     if fieldValue == "" {
       fmt.Printf("[ERROR]: Field '%s' in config file is empty\n", v.Type().Field(i).Name)
-      return
+      os.Exit(1)
     }
   }
 }
@@ -79,6 +96,9 @@ func form() {
 }
 
 func form_projectName() {
+  if cfg.ProjectName != "" {
+    return
+  }
   validate := func(input string) error {
     if len(input) <= 1 {
       return errors.New("Project name must have more than 1 character")
@@ -92,7 +112,7 @@ func form_projectName() {
   result, err := prompt.Run()
   if err != nil {
     fmt.Printf("[ERROR]: Prompt failed %v\n", err)
-    return
+    os.Exit(1)
   }
   cfg.ProjectName = result
 }
@@ -105,7 +125,7 @@ func form_geometryType() {
   _, result, err := prompt.Run()
   if err != nil {
     fmt.Printf("[ERROR]: Prompt failed %v\n", err)
-    return
+    os.Exit(1)
   }
   cfg.GeometryType = result
 }

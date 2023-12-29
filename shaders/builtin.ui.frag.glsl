a898333ff1205f61e85d8cfe70d648dc080d2439
iWas-Coder <wasymatieh01@gmail.com>
2023-12-29 04:04:28 +0100

    UI shader modules

---
+++ shaders/builtin.ui.frag.glsl
@@ -0,0 +1,38 @@
+/*
+ * GNU WGE --- Wildebeest Game Engine™
+ * Copyright (C) 2023 Wasym A. Alonso
+ *
+ * This file is part of WGE.
+ *
+ * WGE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WGE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WGE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#version 450
+
+layout(location = 0) flat in int in_mode;
+layout(location = 1) in struct dto {
+  vec2 texcoord;
+} in_dto;
+
+layout(set = 1, binding = 0) uniform local_uniform_object {
+  vec4 diffuse_color;
+} object_ubo;
+layout(set = 1, binding = 1) uniform sampler2D diffuse_sampler;
+
+layout(location = 0) out vec4 out_color;
+
+void main(void) {
+  out_color = object_ubo.diffuse_color * texture(diffuse_sampler, in_dto.texcoord);
+}
---
+++ shaders/builtin.ui.vert.glsl
@@ -0,0 +1,43 @@
+/*
+ * GNU WGE --- Wildebeest Game Engine™
+ * Copyright (C) 2023 Wasym A. Alonso
+ *
+ * This file is part of WGE.
+ *
+ * WGE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WGE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WGE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#version 450
+
+layout(location = 0) in vec2 in_pos;
+layout(location = 1) in vec2 in_texcoord;
+
+layout(set = 0, binding = 0) uniform global_uniform_obj {
+  mat4 proj;
+  mat4 view;
+} global_ubo;
+layout(push_constant) uniform push_constants {
+  mat4 model;
+} u_push_constants;
+
+layout(location = 0) out int out_mode;
+layout(location = 1) out struct dto {
+  vec2 texcoord;
+} out_dto;
+
+void main(void) {
+  out_dto.texcoord = vec2(in_texcoord.x, 1.0 - in_texcoord.y);
+  gl_Position = global_ubo.proj * global_ubo.view * u_push_constants.model * vec4(in_pos, 0.0, 1.0);
+}

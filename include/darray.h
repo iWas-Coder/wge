a4e6f6eff04889f3238b944be7ac209e2a931f0b
iWas-Coder <wasymatieh01@gmail.com>
2024-02-10 23:05:29 +0100

    fix: formatting issues

--- include/darray.h
+++ include/darray.h
@@ -34,17 +34,21 @@ enum {
 };
 
 KAPI void *_darray_create(u64 length, u64 stride);
+
 KAPI void _darray_destroy(void *array);
 
 KAPI u64 _darray_field_get(void *array, u64 field);
+
 KAPI void _darray_field_set(void *array, u64 field, u64 value);
 
 KAPI void *_darray_resize(void *array);
 
 KAPI void *_darray_push(void *array, const void *value_ptr);
+
 KAPI void _darray_pop(void *array, void *dest);
 
 KAPI void *_darray_insert_at(void *array, u64 index, void *value_ptr);
+
 KAPI void *_darray_pop_at(void *array, u64 index, void *dest);
 
 #define darray_create(type) _darray_create(DARRAY_DEFAULT_CAPACITY, sizeof(type))
--- include/input.h
+++ include/input.h
@@ -153,23 +153,37 @@ typedef enum {
 } keys;
 
 void input_system_initialize(u64* memory_requirements, void* state);
+
 void input_system_shutdown(void *state);
+
 void input_update(f64 delta_time);
 
 // Keys (keyboard input)
 void input_process_key(keys key, b8 pressed);
+
 KAPI b8 input_is_key_down(keys key);
+
 KAPI b8 input_is_key_up(keys key);
+
 KAPI b8 input_was_key_down(keys key);
+
 KAPI b8 input_was_key_up(keys key);
 
 // Buttons (mouse input)
 void input_process_button(buttons button, b8 pressed);
+
 void input_process_mouse_move(i16 x, i16 y);
+
 void input_process_mouse_wheel(i8 z_delta);
+
 KAPI b8 input_is_button_down(buttons button);
+
 KAPI b8 input_is_button_up(buttons button);
+
 KAPI b8 input_was_button_down(buttons button);
+
 KAPI b8 input_was_button_up(buttons button);
+
 KAPI void input_get_mouse_position(i32 *x, i32 *y);
+
 KAPI void input_get_previous_mouse_position(i32 *x, i32 *y);
